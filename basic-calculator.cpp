f274f43ed1e55bd8cc8d949fc3fae0e16cc4c41d
lrboyer <lrboyer@clemson.edu>
2021-09-28 01:21:04 -0400

    feat: fixed bugs and removed unneccesary files

--- basic-calculator.cpp
+++ basic-calculator.cpp
@@ -8,7 +8,9 @@ int main() {
     cout << "---   This is a basic 5 operation calculator   ---" << endl;
     cout << "---   I can do addition, subtraction, multiplication, division, and modulo   ---" << endl;
     cout << "---   You can also use parenthesis   ---" << endl;
-    cout << "---   Example of correct syntax: 2 + 3 * 4 + ( 5 - 6 + 7 ) % 8   ---" << endl << endl << endl;
+    cout << "---   Example of correct syntax for infix: 2 + 3 * 4 + ( 5 - 6 + 7 ) % 8   ---" << endl << 
+    cout << "---   Example of correct syntax for postfix: 2 3 4 * + 5 6 - 7 + 8 * +  ---" << endl << endl << endl;
+
 
     while (input != 3) {
         input = menu();
